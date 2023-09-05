f3629f12bd477b78508fdbd3c5af44a84778a60d
bryantkrause@gmail.com <bryantkrause@gmail.com>
2023-09-04 20:10:35 -0700

    post man it

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import requests
 from dotenv import load_dotenv, dotenv_values
 import os
+import pandas as pd
 
 load_dotenv()
 config = dotenv_values(".env")
 key = os.environ.get("X-RapidAPI-Key")
 host = os.environ.get("X-RapidAPI-Host")
-print(key)
-print(host)
-url = "https://weatherapi-com.p.rapidapi.com/current.json"
+datalist = []
+url = "https://weatherapi-com.p.rapidapi.com/history.json"
 
-querystring = {"q": "53.1,-0.13"}
+querystring = {
+    		
+            "q": "San Diego",
+            "dt": "2023-09-01"
+			   }
 
 headers = {
 	"X-RapidAPI-Key": key,
@@ -18,5 +22,17 @@ headers = {
 }
 
 response = requests.get(url, headers=headers, params=querystring)
+print(response)
+data = response.json()
 
-print(response.json())
+datalist.append(data['forecast']['forecastday'])
+
+newdatalist = datalist[0][0]
+# print(data)
+# data2 = data.text
+df = pd.json_normalize(data)
+# df = pd.DataFrame(newdatalist)
+# df = pd.read_json(data2)
+# df = pd.read_json(data)
+
+pd.DataFrame(data).to_csv('out.csv',index=False)
